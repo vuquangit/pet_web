11bf7c7ed2e75cd9c78614fdd94651c0598c061f
vvquang <vv.quang@vand.vn>
2024-06-17 17:10:55 +0700

    feat: config vercel deploy

--- vercel.sh
+++ vercel.sh
@@ -4,13 +4,13 @@ VERCEL_ENV="staging"
 
 echo Deploying to: $VERCEL_ENV
 
-if [[ "$VERCEL_ENV" == "production" ]]; then
-     echo "Build production"
+if [ $VERCEL_ENV = "production" ]; then
+  echo "Build production"
   yarn build:prod
-elif [[ "$VERCEL_ENV" == "staging" ]]; then
-     echo "Build staging"
+elif [ $VERCEL_ENV = "staging" ]; then
+  echo "Build staging"
   yarn build:stg
 else
-     echo "Build development"
+  echo "Build development"
   yarn build:dev
 fi
