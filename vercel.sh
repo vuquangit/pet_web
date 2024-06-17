669a6e388018f2725263db046d36563354437ccf
Quang Vu <vuquangit@gmail.com>
2024-06-17 15:46:59 +0700

    feat: check env deploy

--- vercel.sh
+++ vercel.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo $VERCEL_ENV
+
 if [[ $VERCEL_ENV == "production"  ]] ; then
  yarn build:prod
-else
+elif [[ $VERCEL_ENV == "staging"  ]] ; then
  yarn build:stg
+else
+ yarn build:dev
 fi
