1155d00cef0adc320dc9992a41156546e51f947a
vvquangit <vuquangit@gmail.com>
2024-09-30 10:25:27 +0700

    feat: log env socket

--- src/context/SocketContext.tsx
+++ src/context/SocketContext.tsx
@@ -7,6 +7,8 @@ import StorageService from '@/services/local-storage'
 const APP_WEBSOCKET_URL = process.env?.APP_WEBSOCKET_URL || 'http://localhost:8080'
 const accessToken = StorageService.get(storageKeys.AUTH_PROFILE)?.accessToken || ''
 
+console.log('APP_WEBSOCKET_URL:', APP_WEBSOCKET_URL)
+
 export const socket = io(APP_WEBSOCKET_URL, {
   withCredentials: true,
   extraHeaders: {
